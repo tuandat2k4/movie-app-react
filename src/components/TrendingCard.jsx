c4d11705f2ac06442774df5d8910fc3d8ba1dd7c
dat26-04 <tdat96386@gmail.com>
2026-07-30 17:21:55 +0700

    ui: fix movie poster

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -17,10 +17,10 @@ const TrendingCard = ({ movie, index, setSelectedMovie }) => {
           src={movie.poster}
           alt={movie.title}
           onError={() => setPosterError(true)}
-          className="relative z-10 ml-10 h-[220px] w-[150px] rounded-2xl object-cover"
+          className="relative z-10 ml-5 h-[220px] w-[150px] rounded-2xl object-cover"
         />
       ) : (
-        <div className="relative z-10 ml-10 flex h-[220px] w-[150px] items-center justify-center rounded-2xl bg-[#1A1A2E] text-center text-gray-400">
+        <div className="relative z-10 ml-5 flex h-[220px] w-[150px] items-center justify-center rounded-2xl bg-[#1A1A2E] text-center text-gray-400">
           <div>
             <p className="font-semibold">Poster</p>
             <p>Not Available</p>
